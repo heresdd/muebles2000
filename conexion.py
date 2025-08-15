a5464a48de74961fef800d0ee6550eb76e7e0bd0
heresdd <160204170+heresdd@users.noreply.github.com>
2025-08-15 16:24:24 -0400

    Update conexion.py

--- conexion.py
+++ conexion.py
@@ -1,11 +1,13 @@
 import os
 import psycopg2
-from psycopg2.extras import RealDictCursor
+import urllib.parse
 
-def connect_to_db():
-    try:
-        conn = psycopg2.connect(os.getenv('DATABASE_URL'))
-        return conn
-    except psycopg2.Error as err:
-        print(f"Error de conexión a la base de datos: {err}")
-        raise err
+def conectar():
+    # Obtén la URL de la base de datos de las variables de entorno
+    url = os.environ.get('DATABASE_URL')
+    if url is None:
+        # Aquí puedes poner tus credenciales locales para desarrollo
+        raise ValueError("DATABASE_URL no está configurada")
+
+    # Conéctate usando la URL
+    return psycopg2.connect(url)
